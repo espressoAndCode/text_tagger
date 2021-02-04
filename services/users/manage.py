a14e69e3b74a47bc38d43e9b4d184da6eefdb40f
Wes Bailey <wes@wesbailey.com>
2021-02-04 06:38:02 -0800

    Starter app - Docker, React, Flask, Postgres

---
+++ services/users/manage.py
@@ -0,0 +1,31 @@
+# manage.py
+
+
+import sys
+
+from flask.cli import FlaskGroup
+
+from src import create_app, db
+from src.api.users.models import User
+
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
+
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command('seed_db')
+def seed_db():
+    db.session.add(User(username='Wes', email="jwb4@clemson.edu", password="hcc8500"))
+    db.session.add(User(username='Meghan', email="mmpatil@g.clemson.edu", password="hcc8500"))
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    cli()
